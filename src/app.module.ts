b68e137c93b6b258ed671566cdc75bbeaecb26ab
Pere Prior <pj.prior.es@gmail.com>
2026-09-12 19:09:53 +0200

    feat: add nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: ['.env']
+    })
+  ],
 })
 export class AppModule {}
